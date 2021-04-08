833379b83dc54e901507eef4fad3c4ce7bf475cf
KalmarCoDna <82134085+KalmarCoDna@users.noreply.github.com>
2021-04-08 14:43:29 +0700

    Initial commit

---
+++ АфоризмыООП/Header.h
@@ -0,0 +1,62 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//Класс кладезей знаний
+class Storehouse {
+public:
+    static Storehouse* In(ifstream& ifst); //Функция ввода информации о кладезе знаний
+    virtual void In_Data(ifstream& ifst) = 0; //Чисто вирутальная функция ввода информации
+                                              //о кладезе, она будет определена для
+                                              //каждого класса кладезя
+    virtual void Out_Data(ofstream& ofst) = 0; //Чисто вирутальная функция вывода информации
+                                              //о кладезе, она будет определена для
+                                              //каждого класса кладезя
+protected:
+    Storehouse() {};
+};
+
+//Класс афоризмов
+class Aphorism : public Storehouse {
+    string Content; //Содержание афоризма
+    string Author; //Автора афоризма
+public:
+    void In_Data(ifstream& ifst); //Функция ввода информации об афоризме
+    void Out_Data(ofstream& ofst); //Функция вывода информации об афоризме
+    Aphorism() {};
+};
+
+//Класс поговорок и пословиц
+class Proverb : public Storehouse {
+    string Content; //Содержание пословицы или поговорки
+    string Country; //Страна пословицы или поговорки
+public:
+    void In_Data(ifstream& ifst); //Функция ввода информации о поговорке или пословице
+    void Out_Data(ofstream& ofst); //Функция вывода информации о поговорке или пословице
+    Proverb() {};
+};
+
+//Узел контейнера
+struct Node {
+    Node* Next; //Указатель на следующий узел
+    Storehouse* Cont; //Указатель на класс кладезей
+};
+
+//Контейнер
+class Container {
+    Node* Head; //Указатель на начало списка
+    int Len; //Размерность контейнера
+public:
+    void In(ifstream& ifst); //Функция ввода элемента в контейнер
+    void Out(ofstream& ofst); //Функция вывода элемента из контейнера
+    void Clear(); //Функция очищения контейнера
+    Container(); //Конструктор (по сути инициализатор контейнера)
+    ~Container() { Clear(); } //Деструктор
+};
+
+#endif //HEADER_H
---
+++ АфоризмыООП/Source.cpp
@@ -0,0 +1,135 @@
+#include "Header.h"
+
+Container::Container() {
+    Head = new Node();
+    Head->Cont = NULL;
+    Head->Next = Head;
+}
+
+void Container::In(ifstream& ifst) {
+    Node* Temp, * P;
+
+    while (!ifst.eof())
+    {
+        Temp = new Node();
+
+        if (Len == 0)
+        {
+            if ((Head->Cont = Storehouse::In(ifst)) != 0)
+            {
+                Len++;
+            }
+        }
+        else
+        {
+            if ((Temp->Cont = Storehouse::In(ifst)) != 0)
+            {
+                P = Head->Next;
+                Head->Next = Temp;
+                Temp->Next = P;
+                Head = Temp;
+                Len++;
+            }
+        }
+    }
+}
+
+Storehouse* Storehouse::In(ifstream& ifst) {
+    Storehouse* St;
+    int K;
+    ifst >> K;
+    if (K == 1)
+    {
+        St = new Aphorism;
+    }
+    else if (K == 2)
+    {
+        St = new Proverb;
+    }
+    else
+    {
+        return 0;
+    }
+
+    St->In_Data(ifst);
+
+    return St;
+}
+
+void Aphorism::In_Data(ifstream& ifst) {
+    string Temp_El = ""; //Буфер для считывания строк
+
+    //Считываем содержание афоризма
+    while (!(ifst >> Temp_El) || (ifst.peek() != '\n'))
+    {
+        Content += Temp_El + " ";
+    }
+
+    Content += Temp_El;
+
+    //Считываем автора афоризма
+    while (!(ifst >> Temp_El) || (ifst.peek() != '\n'))
+    {
+        Author += Temp_El + " ";
+    }
+
+    Author += Temp_El;
+}
+
+void Proverb::In_Data(ifstream& ifst) {
+    string Temp_El = ""; //Буфер для считывания строк
+
+    //Считываем содержание
+    while (!(ifst >> Temp_El) || (ifst.peek() != '\n'))
+    {
+        Content += Temp_El + " ";
+    }
+
+    Content += Temp_El;
+
+    //Считываем страну
+    while (!(ifst >> Temp_El) || (ifst.peek() != '\n'))
+    {
+        Country += Temp_El + " ";
+    }
+
+    Country += Temp_El;
+}
+
+void Container::Out(ofstream& ofst) {
+    ofst << "Container contains " << Len
+        << " elements." << endl;
+
+    if (Head != NULL)
+    {
+        for (int i = 0; i < Len; i++)
+        {
+            ofst << i << ": ";
+            Head->Cont->Out_Data(ofst);
+            Head = Head->Next;
+        }
+    }
+}
+
+void Aphorism::Out_Data(ofstream& ofst) {
+    ofst << "It's an Aphorism: " << Content << endl; //Выводим содержание
+    ofst << "Aphorism's author is: " << Author << endl; //Выводим автора
+}
+
+void Proverb::Out_Data(ofstream& ofst) {
+    ofst << "It's a Proverb: " << Content << endl; //Выводим содержание
+    ofst << "Proverbs's country is: " << Country << endl; //Выводим страну
+}
+
+void Container::Clear() {
+    if (Head != NULL)
+    {
+        while (Len > 0)
+        {
+            free(Head->Cont);
+            Head = Head->Next;
+            Len--;
+        }
+        Head = NULL;
+    }
+}
